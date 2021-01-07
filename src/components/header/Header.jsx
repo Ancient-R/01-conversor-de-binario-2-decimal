ed2869dbb56af88de4885cd99e1b22aa04a87e42
Ancient-R <navalimaricardo@gmail.com>
2021-01-07 16:50:27 -0600

    Construyendo el header

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className="header">Conversor de Binario de Decimal</h1>
+        </header>
+    );
+}
+ 
+export default Header;
