dca7a27cd2fbaadf6f0b1c1c9d884e2781e4a3e7
Ancient-R <navalimaricardo@gmail.com>
2021-01-09 14:00:34 -0600

    Construyendo la funcion de conversion

--- src/helpers/calculateDecimal.js
+++ src/helpers/calculateDecimal.js
@@ -1,4 +1,12 @@
 export const calculateDecimal = value => {
 
-    console.log( value );
+    const arrayNumbers = value.split('').reverse();
+    const arrayMultiply = [];
+    for( let i = 0; i<arrayNumbers.length; i++){
+        arrayMultiply.push( Math.pow(2, i) * arrayNumbers[i] );
+    }
+
+    const decimal = arrayMultiply.reduce( (a, b ) => a+b );
+
+    return decimal;
 }
