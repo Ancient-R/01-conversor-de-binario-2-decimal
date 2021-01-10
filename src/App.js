3a6634ee7ba5ca0dd853ede5a18b56a475e392d5
Ancient-R <navalimaricardo@gmail.com>
2021-01-10 13:23:47 -0600

    Implementacion de los formularios

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/header/Header';
 import Form from './components/ui/Form';
+import FormDecimaltoBinary from './components/ui/FormDecimaltoBinary';
 
 function App() {
   return (
-    <>
+    <div className="container">
       <Form />
-    </>
+      <FormDecimaltoBinary />
+    </div>
   );
 }
 
